22a4412fd71a1b49dc11487d7f9bb3825c49fb7f
ShrushtiKamate <shrushtivkamate@gmail.com>
2026-08-16 23:24:37 +0530

    Update project functionality

---
+++ inspection_system/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,redirect
+
+def index(request):
+    college_name = request.GET.get('college_name', 'Guest')  # Fallback to 'Guest' if not provided
+    return render(request, 'index.html', {'college_name': college_name})
+
+def upload_certificate(request):
+    college_name = request.GET.get('college_name', 'Guest')
+    return render(request, 'index.html', {'college_name': college_name})
